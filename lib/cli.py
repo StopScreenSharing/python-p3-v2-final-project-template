108cd5b2c501bd79eabe95294d1f6b8a29357538
aidadieudonne <aidaharing10@gmail.com>
2025-09-15 06:14:30 -0600

    adding personalized cli

--- lib/cli.py
+++ lib/cli.py
@@ -2,27 +2,22 @@
 
 from helpers import (
     exit_program,
-    helper_1
+    main_menu,
+    gardener_menu
 )
 
 
 def main():
     while True:
-        menu()
+        main_menu()
         choice = input("> ")
+
         if choice == "0":
             exit_program()
         elif choice == "1":
-            helper_1()
+            gardener_menu()
         else:
-            print("Invalid choice")
-
-
-def menu():
-    print("Please select an option:")
-    print("0. Exit the program")
-    print("1. Some useful function")
-
+            print("Invalid choice, try again!")
 
 if __name__ == "__main__":
     main()
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,7 +1,21 @@
 # lib/helpers.py
 
-def helper_1():
-    print("Performing useful function#1.")
+from models.gardener import Gardener
+from models.plant import Plant
+
+def main_menu():
+    print("\n🌱 Gardening Business Manager 🌱")
+    print("0. Exit")
+    print("1. Manage Gardeners")
+
+    def gardener_menu():
+        print("\n🧑‍🌾 Gardener Menu")
+        print("1. View all gardeners")
+        print("2. Add a gardener")
+        print("3. Update a gardener")
+        print("4. Delete a gardener")
+        print("5. Back")
+        
 
 
 def exit_program():
