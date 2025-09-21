5a26054669238519c9864271daf88b9570d55a48
aidadieudonne <aidaharing10@gmail.com>
2025-09-21 07:56:04 -0600

    plant helpers

--- lib/helpers.py
+++ lib/helpers.py
@@ -85,6 +85,40 @@ def gardener_menu():
             print("Invalid choice! Please enter 1-5.")
 
 def plants_menu():
+     while True:
+         gardeners = Gardener.get_all()
+         if not gardeners:
+             print("\nNo gardeners yet -- add a gardener first!")
+             return
+         print("\n🌿 Plants Menu")
+         
+         for i, g in enumerate(gardeners, starts=1):
+             print(f"{i}. {g.name}")
+             print(f"{len(gardeners)+1}. Back")
+
+             g_choice = input("Choose a gardener to manage plants: ").strip()
+             if not g_choice.isdigit() or not (1 <=(g_choice) <= len(gardeners)+1):
+                 print("Invalid selection.")
+                 continue
+             
+             if int(g_choice) == len(gardeners)+1:
+                 break
+             
+             gardener = gardeners[int(g_choice) - 1]
+
+             while True:
+                 print(f"\n🌱 Managing plants for {gardener.name}")
+                 print("1. View plants")
+                 print("2. Add plant")
+                 print("3. Update plant")
+                 print("4. Delete plant")
+                 print("5. Back")
+                 
+
+
+
+        
+        
      print("\n🌸 Plant Menu")
      print("1. View all plants")
      print("2. Add a plant")
